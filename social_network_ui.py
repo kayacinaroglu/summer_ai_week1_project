c8903ffc8fa382c9e67aa1df2afda3c9e913e8ea
kayacinaroglu <75301389+kayacinaroglu@users.noreply.github.com>
2023-06-30 16:05:01 -0400

    added

--- social_network_ui.py
+++ social_network_ui.py
@@ -4,7 +4,7 @@ def mainMenu():
     print("")
     print("1. Create a new account")
     print("2. Manage my account")
-    print("3. Quit")
+    print("3. Save and quit")
     print("********************************************************")
     return input("Please Choose a number: ")
 
@@ -14,5 +14,15 @@ def manageAccountMenu():
     print("2. Add a friend")
     print("3. View all my friends")
     print("4. View all my messages")
-    print("5. <- Go back ")
+    print("5. Send message")
+    print("6. Block a friend")
+    print("7. <- Go back ")
+    return input("Please Choose a number: ")
+
+def editDetailsMenu():
+    print("")
+    print("1. Change username")
+    print("2. Change age")
+    print("3. Password")
+    print("4. <- Go back ")
     return input("Please Choose a number: ")
