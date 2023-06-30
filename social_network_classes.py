c84ff9866e810f537152fa54eea5e42c33568849
kayacinaroglu <75301389+kayacinaroglu@users.noreply.github.com>
2023-06-30 16:05:11 -0400

    added

--- social_network_classes.py
+++ social_network_classes.py
@@ -1,3 +1,5 @@
+import json
+
 # A class to hold general system wide social media data and functions. Eg Data objects of all people, Eg functions: Save social media to disk
 class SocialNetwork:
     def __init__(self):
@@ -6,33 +8,77 @@ class SocialNetwork:
         
     ## For more challenge try this
     def save_social_media(self):
-        # function to save social media to a file on disk 
-        # hint: look up how to use python's inbuil json module to turn objects to json
-        # you can write this json unto a file on disk
-        pass
+        listObj = []
+        for user in self.list_of_people:
+            dictionary = {"name":user.name,"password":user.password,"age":user.age,"friends": user.friendlist,"inbox":user.inbox,"blocked":user.blocked}
+            listObj.append(dictionary)
+        with open("sample.json", "w") as outfile:
+            json.dump(listObj, outfile, indent=4)
+
+        
+
 
     ## For more challenge try this
-    def reload_social_media(self):
-        # function to load saved social media from file on disk 
-        # hint: load a the json file from disk and look up how to recreate the list of people objects.
-        pass
+    def reload_social_media(self, network):
+        with open("sample.json", "r") as outfile:
+            json_object = json.load(outfile)
+        for i, (name, password, age, friends, inbox, blocked) in enumerate(json_object):
+           network.list_of_people.append(Person("Adam",1293821, 15))
+
+
+
 
     def  create_account(self):
-        #implement function that creates account here
-        print("Creating ...")
-        pass
+        
+        name = input("Enter username for account: ")
+        password = input("Enter password for account: ")
+        age = input("Enter age for account: ")
+        return name, age, password
+
 
 
 class Person:
-    def __init__(self, name, age):
-        self.id = name
-        self.year = age
+    def __init__(self, name, age, password):
+        self.name = name
+        self.age = age
+        self.password = password
         self.friendlist = []
+        self.inbox = []
+        self.blocked = []
 
-    def add_friend(self, person_object):
-        #implement adding friend. Hint add to self.friendlist
-        pass
+    def add_friend(self, network):
+        person_name = input("Enter name of person: ")
+        if person_name in [network.list_of_people[n].name for n, i in enumerate(network.list_of_people)]:
+            self.friendlist.append(person_name)
+            print(person_name + " added.")
+        else: 
+            print("Person doesn't exist")
+        
 
-    def send_message(self):
-        #implement sending message to friend here
-        pass
+    def send_message(self, network):
+        friend_name = input("Who's the message for: ")
+        if friend_name in [network.list_of_people[n].name for n, i in enumerate(network.list_of_people)]:
+            message = input("Enter message: ")
+            self.inbox.append([self.name, friend_name, message])
+            print("The message " + message + " was sent to " + friend_name)
+        else: 
+            print("Person doesn't exist")
+
+        
+
+    def view_friends(self):
+        print(self.friendlist)
+    
+    def check_messages(self):
+        print("==== Recent Messages ===")
+        for i in self.inbox:
+            print(i)
+
+    def block_friends(self, network):
+        friend_name = input("Who you wanna block: ")
+        if friend_name in [network.list_of_people[n].name for n, i in enumerate(network.list_of_people)]:
+            self.friendlist.remove(friend_name)
+            self.blocked.append(friend_name)
+            print(friend_name + " blocked")
+        else: 
+            print("Person doesn't exist")
